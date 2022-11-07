7a849a7ca739cc408fae3f8421a72972194629b3
Fuyula <fuyulanodahijos@gmail.com>
2022-11-07 20:51:34 -0300

    fix: fixed page not found lottie animation responsiveness

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Lottie from 'react-lottie';
-import animationData from '../assets/404.json'
+import animationData from '../assets/404.json';
+import '../styles/pages/page-not-found.scss';
 
 const PageNotFound = () => {
     const defaultOptions = {
@@ -15,11 +16,9 @@ const PageNotFound = () => {
     <div className='container-fluid d-flex flex-column align-items-center justify-content-center'>
         <div className='container-fluid d-flex flex-column align-items-center justify-content-center' style={{ minHeight: '80vh'}}>
             <h1 className='display-1 m-5'>PageNotFound </h1>
-            <div className='container-fluid d-flex flex-column align-items-center justify-content-center'>
+            <div className='container-fluid d-flex flex-column align-items-center justify-content-center' id = "lottie-animation">
                 <Lottie 
                     options={defaultOptions}
-                    height={400}
-                    width={600}
                 />
             </div>
         </div>
